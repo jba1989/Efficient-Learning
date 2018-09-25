b83c4d98a15032c0c9eaefb0b4f3ddbd4028a766
jba1989 <jba1989@gmail.com>
2018-09-25 21:05:34 +0800

    class路由

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/total', 'ClassController@classList')->name('total');
+Route::get('/school/{school}', 'ClassController@classOfSchool')->name('school');
+Route::get('/Class/{className}', 'ClassController@classOfSchool')->name('class');
