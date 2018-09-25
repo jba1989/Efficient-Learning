2a0443350d20d8bca34af2f8e5b6191a3a854526
jba1989 <jba1989@gmail.com>
2018-09-25 12:27:08 +0800

    新增MemberController

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class MemberController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function member()
+    {
+        $userId = Auth::id();
+        $data = DB::table('users')->where('id', userId)->get();
+        return view('/mooc/member', $data);
+    }
+
+    public function favorite()
+    {
+        $userId = Auth::id();
+        $favoriteStr = DB::table('users')->where('id', userId)->value('favorite');
+        $favoriteIdArr = explode($favoriteStr, ',');
+        $favoriteClass = DB::table('class_list')->select('classId', 'className')
+            ->whereIn('classId', $favoriteIdArr)->get()->orderBy('asc');
+        return view('/mooc/favorite', $favoriteClass);
+    }
+}
