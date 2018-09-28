7e0dc3d7e99bbeafa16a184db071dea0690a68e5
jba1989 <jba1989@gmail.com>
2018-09-28 23:35:38 +0800

    修改classList 模板

--- resources/views/mooc/classList.blade.php
+++ resources/views/mooc/classList.blade.php
@@ -22,19 +22,68 @@
 @endsection
 
 @section('content')
-    <ul class="nav nav-tabs">
-        <li class="nav-item">
-            <a class="nav-link active" href="#">Active</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-        </li>
-    </ul>
+
+    <!-- classType -->
+    <section id="three" class="wrapper">
+        <div class="inner bg-orange flex flex-3">
+            <div class="flex-item box">
+                <div class="image fit">
+                    <a href="NTU_class.php?classtype=熱門點閱"><img src="images/type1.png" alt="" /></a>
+                </div>
+                <div class="content">
+                    <h3><a href="NTU_class.php?classtype=熱門點閱">熱門點閱</a></h3>
+                    <p style="color:red;">最新功能!!!</p>
+                </div>
+            </div>
+            <div class="flex-item box">
+                <div class="image fit">
+                    <a href="NTU_class.php?classtype=理工電資"><img src="images/type3.png" alt="" /></a>
+                </div>
+                <div class="content">
+                    <h3><a href="NTU_class.php?classtype=理工電資">理工電資</a></h3>
+                    <p></p>
+                </div>
+            </div>
+            <div class="flex-item box">
+                <div class="image fit">
+                    <a href="NTU_class.php?classtype=法社管理"><img src="images/type2.png" alt="" /></a>
+                </div>
+                <div class="content">
+                    <h3><a href="NTU_class.php?classtype=法社管理">法社管理</a></h3>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- 課程表 -->
+    <section id="main" class="wrapper">
+        <div class="inner">
+            <h3>課程表</h3>
+            <div class="table-wrapper">
+                <table id="classList">
+                    <thead>
+                    <tr>
+                        <th style="text-align:center">課程ID:</th>
+                        <th>課程名稱:</th>
+                        <th style="text-align:center">讚數:</th>
+                        <th>開課教授:</th>
+                        <th>課程分類:</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
 
 @endsection
