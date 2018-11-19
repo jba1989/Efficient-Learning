c94c8d3a328a9cd3eff4404bcb08de456087b7ba
jba1989 <jba1989@gmail.com>
2018-11-19 14:17:35 +0800

    合併class controller/repository功能

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -3,21 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Services\ClassService;
 use App\Models\ClassList;
+use App\Models\TotalClass;
 use Validator;
 
 class ClassController extends Controller
-{
-    protected $classService;
-    public $classOptions;
-
-    /**
-     * 注入repository
-     */    
-    public function __construct(ClassService $classService)
+{   
+    public function __construct()
     {
-        $this->classService = $classService;
         $this->classOptions = ClassList::select('classId', 'className')->get();
     }
 
@@ -37,20 +30,28 @@ class ClassController extends Controller
             'school' => 'nullable|alpha_num|max:12',
             'type' => 'nullable|alpha_num|max:12',
             'classId' => 'nullable|alpha_num|max:12',
+            'msg_page' => 'nullable|integer|max:4',
+            'msg_per_page' => 'nullable|integer|in([25, 50,100])',
         ];
 
         $validator = Validator::make($input, $rules);
 
+        // 返回課程選單
         if ($validator->fails()) {
             return redirect("/class");
         }
 
+        // 進入課程章節選單
         if (isset($input['class'])) {
             $conditions = array('classId' => $input['class']);
 
-            $classes = $this->classService->showClassBy($conditions);
-            $titles = $this->classService->showTitleBy($conditions);
-            $messages = $this->classService->showMessageBy($conditions);
+            // 設定讀取頁數
+            $msg_page = (isset($input['msg_page'])) ? $input['msg_per_page'] : 1;
+            $msg_per_page = (isset($input['msg_per_page'])) ? $input['msg_per_page'] : 25;
+        
+            $classes = ClassList::where($conditions)->orderBy('id', 'asc')->paginate(30);
+            $titles = TotalClass::where($conditions)->orderBy('titleId', 'asc')->paginate(30);
+            $messages = ClassList::find(1)->messages()->where($conditions)->orderBy('id', 'asc')->paginate($msg_per_page, ['*'], $msg_page);
 
             return view('mooc.singleClass', [
                 'classOptions' => $this->classOptions,
@@ -60,6 +61,7 @@ class ClassController extends Controller
             ]);
         }
 
+        // 進入課程選單
         if (isset($input['school']) && $input['school'] != '') {
             $school = $input['school'];
             $conditions = array_merge($conditions, ['school' => $input['school']]);
@@ -70,7 +72,8 @@ class ClassController extends Controller
             $conditions = array_merge($conditions, ['classType' => $input['type']]);
         }
 
-        $classes = $this->classService->showClassBy($conditions);
+        $classes = ClassList::where($conditions)->orderBy('id', 'asc')->paginate(30);
+        
         return view('mooc.classList', [
             'classOptions' => $this->classOptions,
             'classes' => $classes,
