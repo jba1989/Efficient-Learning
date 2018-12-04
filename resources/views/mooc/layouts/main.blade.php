92eae3719dff952083291cafaee0c95009c2c9b4
jba1989 <jba1989@gmail.com>
2018-12-04 09:34:00 +0800

    修正引用字典檔名稱錯誤

--- resources/views/mooc/layouts/main.blade.php
+++ resources/views/mooc/layouts/main.blade.php
@@ -2,7 +2,7 @@
 
 <html>
 	<head>
-		<title>{{ __('dictionary.Web site Name') }}</title>
+		<title>{{ __('dictionary.Website Name') }}</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
