25b27386e1494347c106119fe8da92189fa98db3
jba1989 <jba1989@gmail.com>
2018-12-09 23:53:03 +0800

    修正like dislike圖片錯誤,member.blade我的最愛顯示錯誤

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -13,15 +13,20 @@ class MemberController extends Controller
      *
      * @return void
      */
+    /*
     public function __construct()
     {
         $this->middleware('auth');
     }
-
+*/
     public function userInfo()
     {
-        $favorites = ClassList::select('classId', 'className')->whereIn('classId', Auth::user()->favorite)->get();
-
-        return view('/mooc/member', ['favorites' => $favorites]);
+        if (Auth::user()->favorite != null) {
+            $favorites = ClassList::select('classId', 'className')->whereIn('classId', Auth::user()->favorite)->get()->toArray();
+            
+            return view('/mooc/member', ['favorites' => $favorites]);
+        } else {
+            return view('/mooc/member');
+        }
     }
 }
