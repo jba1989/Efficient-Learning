305af1009b6494be53ab22b649b1fa0b4cc527c5
jba1989 <jba1989@gmail.com>
2018-12-19 12:33:04 +0800

    更新排程設定,更新config.mail設定

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Mail;
+use Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,8 +27,61 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('update:ntu')->weekly()->withoutOverlapping();;
-        $schedule->command('update:nctu')->weekly()->withoutOverlapping();;
+        // 維護開始
+        $schedule->command('down')->weekly()->mondays()->at('3:00')->after(function () {
+            Log::info('維護開始');
+            Mail::raw('維護開始', function($message) {
+                $message->to('jba1989@gmail.com');
+                $message->subject('維護開始');
+            });
+        });
+
+        // 更新台大課程
+        $schedule
+            ->command('update:ntu')
+            ->weekly()->mondays()->at('3:10')
+            ->before(function () {
+                Log::info('開始更新台大課程');
+                Mail::raw('開始更新台大課程', function($message) {
+                    $message->to('jba1989@gmail.com');
+                    $message->subject('開始更新台大課程');
+                });
+            })
+            ->after(function () {
+                Log::info('完成更新台大課程');
+                Mail::raw('完成更新台大課程', function($message) {
+                    $message->to('jba1989@gmail.com');
+                    $message->subject('完成更新台大課程');
+                });
+            });
+
+        // 更新交大課程
+        $schedule
+            ->command('update:nctu')
+            ->weekly()->mondays()->at('3:10')
+            ->before(function () {
+                Log::info('開始更新台大課程');
+                Mail::raw('開始更新交大課程', function($message) {
+                    $message->to('jba1989@gmail.com');
+                    $message->subject('開始更新交大課程');
+                });
+            })
+            ->after(function () {
+                Log::info('完成更新交大課程');
+                Mail::raw('完成更新交大課程', function($message) {
+                    $message->to('jba1989@gmail.com');
+                    $message->subject('開始更新交大課程');
+                });
+            });
+
+        // 維護結束
+        $schedule->command('down')->weekly()->mondays()->at('4:30')->after(function () {
+            Log::info('維護結束');
+            Mail::raw('維護結束', function($message) {
+                $message->to('jba1989@gmail.com');
+                $message->subject('維護結束');
+            });
+        });
     }
 
     /**
